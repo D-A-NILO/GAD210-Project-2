a14b3af3765195b36c7008af13930fe677d801cb
Danilo Miguel P. Gabriel <danilo.gabriel@saedubai.com>
2025-11-21 04:23:06 +0400

    the arena can now periodically flash a global light temporarily
    -added an arena
    -added flashing light script to global light 2d
    -set the input system to both

---
+++ GAD 210 Project 2/Assets/Scripts/LightFlash.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightFlash : MonoBehaviour
+{
+    public Light2D globalLight;
+    public float flashDuration;
+    public float flashTimer;
+    private bool canFlash;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canFlash = true;
+        StartCoroutine(Flash());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(Flash());
+        }
+
+        
+    }
+
+    IEnumerator Flash()
+    {
+        yield return new WaitForSeconds(flashTimer);
+
+        while (canFlash == true)
+        {
+            canFlash = false;
+            globalLight.intensity = 1;
+            yield return new WaitForSeconds(flashDuration);
+            globalLight.intensity = 0;
+
+            yield return new WaitForSeconds(flashTimer);
+            canFlash = true;
+        }
+    }
+}
