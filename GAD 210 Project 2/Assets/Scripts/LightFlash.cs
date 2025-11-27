96babb8a6291a9c5635384ad780d7893cbe099fa
Danilo Miguel P. Gabriel <danilo.gabriel@saedubai.com>
2025-11-27 17:16:06 +0400

    removed temp light flash button
    -also added gitignore

--- GAD 210 Project 2/Assets/Scripts/LightFlash.cs
+++ GAD 210 Project 2/Assets/Scripts/LightFlash.cs
@@ -19,11 +19,6 @@ public class LightFlash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            StartCoroutine(Flash());
-        }
-
         
     }
 
